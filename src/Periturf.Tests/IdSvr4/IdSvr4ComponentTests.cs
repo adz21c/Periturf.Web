68e797a8dd7ccdbf9d54df33cf52578e737179f5
Adam Burton <adz21c@gmail.com>
2019-06-29 14:10:02 +0100

    Test configuration applies and can be removed

--- src/Periturf.Tests/IdSvr4/IdSvr4ComponentTests.cs
+++ src/Periturf.Tests/IdSvr4/IdSvr4ComponentTests.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using Periturf.IdSvr4;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,11 +16,18 @@ namespace Periturf.Tests.IdSvr4
         // TODO: Name tests
 
         [Test]
-        public async Task Given_Something_When_Something_Then_SomethingAsync()
+        public async Task Given_Configuration_When_QueryClientAndResourceStores_Then_ReceiveExpectedResults()
         {
             // Arrange
             const string enabledClientId = "Client1";
             const string disabledClientId = "Client2";
+            const string fakeClientId = "FakeClient";
+            const string apiResource = "API";
+            const string fakeApiResource = "Fake_API";
+            const string apiScope = "API_Scope";
+            const string fakeApiScope = "Fake_API_Scope";
+            const string idResource = "ID";
+            const string fakeIdResource = "FakeID";
 
             var config = new ConfigurationRegistration(
                 new List<Client>
@@ -27,8 +35,20 @@ namespace Periturf.Tests.IdSvr4
                     new Client { ClientId = enabledClientId, Enabled = true },
                     new Client { ClientId = disabledClientId, Enabled = false }
                 },
-                new List<IdentityResource>(),
-                new List<ApiResource>());
+                new List<IdentityResource>
+                {
+                    new IdentityResource { Name = idResource }
+                },
+                new List<ApiResource>
+                {
+                    new ApiResource(apiResource)
+                    {
+                        Scopes = new List<Scope>
+                        {
+                            new Scope(apiScope)
+                        }
+                    }
+                });
 
             var component = new IdSvr4Component();
             await component.RegisterConfigurationAsync(Guid.NewGuid(), config);
@@ -39,24 +59,44 @@ namespace Periturf.Tests.IdSvr4
             // Act & Assert
             Assert.NotNull(await clientStore.FindClientByIdAsync(enabledClientId));
             Assert.NotNull(await clientStore.FindClientByIdAsync(disabledClientId));
+            Assert.IsNull(await clientStore.FindClientByIdAsync(fakeClientId));
             Assert.NotNull(await clientStore.FindEnabledClientByIdAsync(enabledClientId));
             Assert.IsNull(await clientStore.FindEnabledClientByIdAsync(disabledClientId));
+            Assert.IsNull(await clientStore.FindEnabledClientByIdAsync(fakeClientId));
 
-            // TODO: Add resource assertions
+            Assert.IsNotNull(await resourceStore.FindApiResourceAsync(apiResource));
+            Assert.IsNull(await resourceStore.FindApiResourceAsync(fakeApiResource));
+            Assert.IsNotEmpty(await resourceStore.FindApiResourcesByScopeAsync(new[] { apiScope }));
+            Assert.IsEmpty(await resourceStore.FindApiResourcesByScopeAsync(new[] { fakeApiScope }));
+            Assert.IsNotEmpty(await resourceStore.FindIdentityResourcesByScopeAsync(new[] { idResource }));
+            Assert.IsEmpty(await resourceStore.FindIdentityResourcesByScopeAsync(new[] { fakeIdResource }));
         }
 
-        public async Task Given_Something_When_Something_Then_SomethingAsync2()
+        [Test]
+        public async Task Given_RegisteredConfiguration_When_UnregisterConfiguration_Then_ConfigurationRemoved()
         {
             // Arrange
             const string enabledClientId = "Client1";
+            const string apiResource = "API";
+            const string apiScope = "API_Scope";
+            const string idResource = "ID";
 
             var config = new ConfigurationRegistration(
                 new List<Client>
                 {
-                    new Client { ClientId = enabledClientId, Enabled = true },
+                    new Client { ClientId = enabledClientId, Enabled = true }
                 },
-                new List<IdentityResource>(),
-                new List<ApiResource>());
+                new List<IdentityResource>
+                {
+                    new IdentityResource { Name = idResource }
+                },
+                new List<ApiResource>
+                {
+                    new ApiResource(apiResource)
+                    {
+                        Scopes = new List<Scope> { new Scope(apiScope) }
+                    }
+                });
 
             var component = new IdSvr4Component();
             var configId = Guid.NewGuid();
@@ -65,14 +105,19 @@ namespace Periturf.Tests.IdSvr4
             var clientStore = (IClientStore)component;
             var resourceStore = (IResourceStore)component;
 
-            // Act & Assert
-            Assert.NotNull(await clientStore.FindClientByIdAsync(enabledClientId));
+            Assume.That((await clientStore.FindClientByIdAsync(enabledClientId)) != null);
+            Assume.That((await resourceStore.FindApiResourceAsync(apiResource)) != null);
+            Assume.That((await resourceStore.FindApiResourcesByScopeAsync(new[] { apiScope })).Any());
+            Assume.That((await resourceStore.FindIdentityResourcesByScopeAsync(new[] { idResource })).Any());
 
-            await component.UnregisterConfigurationAsync(configId);
+            // Act
+            Assert.DoesNotThrow(() => component.UnregisterConfigurationAsync(configId));
 
+            // Assert
             Assert.IsNull(await clientStore.FindClientByIdAsync(enabledClientId));
-
-            // TODO: Add resource assertions
+            Assert.IsNull(await resourceStore.FindApiResourceAsync(apiResource));
+            Assert.IsEmpty(await resourceStore.FindApiResourcesByScopeAsync(new[] { apiScope }));
+            Assert.IsEmpty(await resourceStore.FindIdentityResourcesByScopeAsync(new[] { idResource }));
         }
     }
 }
