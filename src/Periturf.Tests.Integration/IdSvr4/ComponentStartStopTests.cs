c02ac0c42ac6a055a268f8867d59d873654ce89a
Adam Burton <adz21c@gmail.com>
2019-07-02 23:50:02 +0100

    Start and Stop test for AspNetCore host and IdSvr component

---
+++ src/Periturf.Tests.Integration/IdSvr4/ComponentStartStopTests.cs
@@ -0,0 +1,81 @@
+﻿/*
+ *     Copyright 2019 Adam Burton (adz21c@gmail.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using NUnit.Framework;
+using IdentityModel.Client;
+using static IdentityModel.OidcConstants;
+using Microsoft.AspNetCore.Hosting;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Periturf.Tests.Integration.IdSvr4
+{
+    [TestFixture]
+    class ComponentStartStopTests
+    {
+        private const string WebHostUrl = "http://localhost:3501";
+
+        [Test]
+        public async Task Given_IdSvr4_When_StartAndStop_Then_RespondAndDontRespond()
+        {
+            var env = Environment.Setup(e =>
+            {
+                e.WebHost(h =>
+                {
+                    h.UseUrls(WebHostUrl);
+                    h.SetupIdSvr4();
+                });
+            });
+
+            var client = new HttpClient();
+            var beforeStartResponse = await client.RequestTokenAsync(new IdentityModel.Client.TokenRequest
+            {
+                Address = WebHostUrl + "/connect/token",
+                GrantType = GrantTypes.ClientCredentials,
+                ClientId = "Client1",
+                ClientSecret = "Secret"
+            });
+            Assert.IsTrue(beforeStartResponse.IsError);
+            Assert.NotNull(beforeStartResponse.Exception);
+
+            await env.StartAsync();
+
+            var afterStartResponse = await client.RequestTokenAsync(new IdentityModel.Client.TokenRequest
+            {
+                Address = WebHostUrl + "/connect/token",
+                GrantType = GrantTypes.ClientCredentials,
+                ClientId = "Client1",
+                ClientSecret = "Secret"
+            });
+
+            Assert.IsTrue(afterStartResponse.IsError);
+            Assert.AreEqual(HttpStatusCode.BadRequest, afterStartResponse.HttpStatusCode);
+            Assert.AreEqual("invalid_client", afterStartResponse.Error);
+
+            await env.StopAsync();
+
+            var afterStopResponse = await client.RequestTokenAsync(new IdentityModel.Client.TokenRequest
+            {
+                Address = WebHostUrl + "/connect/token",
+                GrantType = GrantTypes.ClientCredentials,
+                ClientId = "Client1",
+                ClientSecret = "Secret"
+            });
+            Assert.IsTrue(afterStopResponse.IsError);
+            Assert.NotNull(afterStopResponse.Exception);
+        }
+    }
+}
