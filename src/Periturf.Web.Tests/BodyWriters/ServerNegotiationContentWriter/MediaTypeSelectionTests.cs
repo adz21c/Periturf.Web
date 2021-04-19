44827faeb9b7ad4cd1df643ccfb1067a16f3e48d
Adam Burton <adz21c@gmail.com>
2021-04-19 05:10:18 +0100

    Accept negotiation

---
+++ src/Periturf.Web.Tests/BodyWriters/ServerNegotiationContentWriter/MediaTypeSelectionTests.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading;
+using System.Threading.Tasks;
+using FakeItEasy;
+using Microsoft.Extensions.Primitives;
+using NUnit.Framework;
+using Periturf.Web.BodyWriters;
+using Periturf.Web.BodyWriters.ContentNegotiation;
+
+namespace Periturf.Web.Tests.BodyWriters.ServerNegotiationContentWriter
+{
+    class MediaTypeSelectionTests
+    {
+        private IWebRequestEvent _event;
+        private IWebResponse _response;
+        private BodyType _dummyBody;
+        private ServerContentNegotiationSpecification _spec;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _event = A.Fake<IWebRequestEvent>();
+            _response = A.Fake<IWebResponse>();
+            _dummyBody = new BodyType();
+
+            _spec = new ServerContentNegotiationSpecification();
+        }
+
+        private IBodyWriter RegisterWriter(string type = null, string subType = null, string suffix = null)
+        {
+            var writer = A.Fake<IBodyWriter>();
+            var writerSpec = A.Fake<IWebBodyWriterSpecification>();
+            A.CallTo(() => writerSpec.Build()).Returns(writer);
+
+            _spec.MediaTypeWriter(c =>
+            {
+                c.Type = type;
+                c.SubType = subType;
+                c.Suffix = suffix;
+                c.AddWebBodyWriterSpecification(writerSpec);
+            });
+
+            return writer;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Fake.ClearConfiguration(_event);
+            Fake.ClearRecordedCalls(_event);
+            Fake.ClearConfiguration(_response);
+            Fake.ClearRecordedCalls(_response);
+        }
+
+        [TestCase("application", "json")]
+        [TestCase("application", "xml")]
+        public async Task Given_MultipleMediaType_When_WriteWithOneMatch_Then_MatchingWriterChosen(string type, string subType)
+        {
+            var headers = new Dictionary<string, StringValues>
+            {
+                { "Accept", new string[]{ "application/json", "application/xml" } }
+            };
+            A.CallTo(() => _event.Request.Headers).Returns(headers.ToImmutableDictionary());
+
+            var writer1 = RegisterWriter(type, subType);
+            var sut = _spec.Build();
+
+            await sut.WriteAsync(_event, _response, _dummyBody, CancellationToken.None);
+
+            A.CallTo(() => writer1.WriteAsync(_event, _response, _dummyBody, A<CancellationToken>._)).MustHaveHappened();
+        }
+
+        [Test]
+        public async Task Given_MultipleMediaType_When_WriteWithMultipleMatch_Then_ChosenInProvidedOrder()
+        {
+            var headers = new Dictionary<string, StringValues>
+            {
+                { "Accept", new string[]{ "application/json", "application/xml" } }
+            };
+            A.CallTo(() => _event.Request.Headers).Returns(headers.ToImmutableDictionary());
+
+            var writer1 = RegisterWriter("application", "json");
+            var writer2 = RegisterWriter("application", "xml");
+            var sut = _spec.Build();
+
+            await sut.WriteAsync(_event, _response, _dummyBody, CancellationToken.None);
+
+            A.CallTo(() => writer1.WriteAsync(_event, _response, _dummyBody, A<CancellationToken>._)).MustHaveHappened();
+            A.CallTo(() => writer2.WriteAsync(A<IWebRequestEvent>._, A<IWebResponse>._, A<BodyType>._, A<CancellationToken>._)).MustNotHaveHappened();
+        }
+
+        [Test]
+        public async Task Given_MultipleMediaTypeWithQFators_When_Write_Then_HighestFactorChosen()
+        {
+            var headers = new Dictionary<string, StringValues>
+            {
+                { "Accept", new string[]{ "application/json; q=0.5", "application/xml; q=0.75" } }
+            };
+            A.CallTo(() => _event.Request.Headers).Returns(headers.ToImmutableDictionary());
+
+            var writer1 = RegisterWriter("application", "json");
+            var writer2 = RegisterWriter("application", "xml");
+            var sut = _spec.Build();
+
+            await sut.WriteAsync(_event, _response, _dummyBody, CancellationToken.None);
+
+            A.CallTo(() => writer1.WriteAsync(A<IWebRequestEvent>._, A<IWebResponse>._, A<BodyType>._, A<CancellationToken>._)).MustNotHaveHappened();
+            A.CallTo(() => writer2.WriteAsync(_event, _response, _dummyBody, A<CancellationToken>._)).MustHaveHappened();
+        }
+
+        [Test]
+        public async Task Given_MultipleMediaTypeWithSameQFators_When_Write_Then_ChosenInProvidedOrder()
+        {
+            var headers = new Dictionary<string, StringValues>
+            {
+                { "Accept", new string[]{ "application/json; q=0.75", "application/xml; q=0.75" } }
+            };
+            A.CallTo(() => _event.Request.Headers).Returns(headers.ToImmutableDictionary());
+
+            var writer1 = RegisterWriter("application", "json");
+            var writer2 = RegisterWriter("application", "xml");
+            var sut = _spec.Build();
+
+            await sut.WriteAsync(_event, _response, _dummyBody, CancellationToken.None);
+
+            A.CallTo(() => writer1.WriteAsync(_event, _response, _dummyBody, A<CancellationToken>._)).MustHaveHappened();
+            A.CallTo(() => writer2.WriteAsync(A<IWebRequestEvent>._, A<IWebResponse>._, A<BodyType>._, A<CancellationToken>._)).MustNotHaveHappened();
+        }
+
+        [Test]
+        public async Task Given_MultipleMediaTypeWithAndWithoutQFators_When_Write_Then_WithoutQFactorTreatedAs1()
+        {
+            var headers = new Dictionary<string, StringValues>
+            {
+                { "Accept", new string[]{ "application/json; q=0.5", "application/xml; q=0.75", "application/yaml" } }
+            };
+            A.CallTo(() => _event.Request.Headers).Returns(headers.ToImmutableDictionary());
+
+            var writer1 = RegisterWriter("application", "json");
+            var writer2 = RegisterWriter("application", "xml");
+            var writer3 = RegisterWriter("application", "yaml");
+            var sut = _spec.Build();
+
+            await sut.WriteAsync(_event, _response, _dummyBody, CancellationToken.None);
+
+            A.CallTo(() => writer1.WriteAsync(A<IWebRequestEvent>._, A<IWebResponse>._, A<BodyType>._, A<CancellationToken>._)).MustNotHaveHappened();
+            A.CallTo(() => writer2.WriteAsync(A<IWebRequestEvent>._, A<IWebResponse>._, A<BodyType>._, A<CancellationToken>._)).MustNotHaveHappened();
+            A.CallTo(() => writer3.WriteAsync(_event, _response, _dummyBody, A<CancellationToken>._)).MustHaveHappened();
+        }
+    }
+}
--- src/Periturf.Web/BodyWriters/ContentNegotiation/ServerContentNegotiationWriter.cs
+++ src/Periturf.Web/BodyWriters/ContentNegotiation/ServerContentNegotiationWriter.cs
@@ -17,6 +17,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Primitives;
 
 namespace Periturf.Web.BodyWriters.ContentNegotiation
 {
@@ -32,43 +33,20 @@ namespace Periturf.Web.BodyWriters.ContentNegotiation
         public async ValueTask WriteAsync<TBody>(IWebRequestEvent @event, IWebResponse response, TBody body, CancellationToken ct) where TBody : class
         {
             @event.Request.Headers.TryGetValue("Accept", out var acceptValues);
-            var target = ToMediaType(acceptValues);
 
-            var query = _writers.AsQueryable();
-
-            if (!string.IsNullOrWhiteSpace(target.Suffix))
-                query = query.Where(x => x.MediaType.Suffix == target.Suffix);
-
-            if (target.Type != null)
+            var weightedAcceptValues = GetWeightedAcceptValues(acceptValues);
+            foreach (var mediaType in weightedAcceptValues)
             {
-                query = query.Where(x => x.MediaType.Type == null || x.MediaType.Type == target.Type);
-                if (target.SubType != null)
-                    query = query.Where(x => x.MediaType.SubType == null || x.MediaType.SubType == target.SubType);
-            }
-
-            var writer = query.Select(x => new
+                var target = ToMediaType(mediaType);
+                var writer = GetMediaTypeWriter(target);
+                if (writer != null)
                 {
-                    x.MediaType,
-                    x.Writer,
-                    TypeMatch = x.MediaType.Type == target.Type,
-                    SubTypeMatch = x.MediaType.SubType == target.SubType
-                })
-                .Select(x =>new
-                {
-                    x.MediaType,
-                    x.Writer,
-                    Score = (x.TypeMatch ? 1 : 0) + (x.SubTypeMatch ? 1 : 0) + (target.Suffix == null && x.MediaType.Suffix == null ? 1 : 0)
-                })
-                .OrderByDescending(x => x.Score)
-                .FirstOrDefault();
-
-            if (writer == default)
-            {
-                response.StatusCode = System.Net.HttpStatusCode.NotAcceptable;
-                return;
+                    await writer.WriteAsync(@event, response, body, ct);
+                    return;
+                }
             }
-
-            await writer.Writer.WriteAsync(@event, response, body, ct);
+            
+            response.StatusCode = System.Net.HttpStatusCode.NotAcceptable;
         }
 
         private MediaType ToMediaType(string value)
@@ -83,6 +61,66 @@ namespace Periturf.Web.BodyWriters.ContentNegotiation
                 SubType = subType == "*" ? null : subType,
                 Suffix = subTypeSplit.Count() > 1 ? subTypeSplit.LastOrDefault() : null
             };
+        }
+        
+        private IEnumerable<string> GetWeightedAcceptValues(StringValues acceptValues)
+        {
+            return acceptValues
+                .Select(x =>
+                {
+                    var split = x.Split(";");
+                    double qfactor = 1;
+                    if (split.Length == 2 && split[1] != null)
+                        double.TryParse(split[1].Split("=")[1].Trim(), out qfactor);
+                    return new
+                    {
+                        MediaType = split[0],
+                        QFactor = qfactor
+                    };
+                })
+                .Zip(
+                    Enumerable.Range(0, acceptValues.Count),
+                    (x, y) => new { x.MediaType, x.QFactor, ProvidedOrder = y })
+                .OrderByDescending(x => x.QFactor).ThenBy(x => x.ProvidedOrder)
+                .Select(x => x.MediaType);
+        }
+
+        private IBodyWriter GetMediaTypeWriter(MediaType target)
+        {
+            var query = _writers.AsQueryable();
+
+            // If suffix provided then exact match required
+            if (!string.IsNullOrWhiteSpace(target.Suffix))
+                query = query.Where(x => x.MediaType.Suffix == target.Suffix);
+
+            if (target.Type != null)
+            {
+                // If type provided then fuzzy or exact match reqired
+                query = query.Where(x => x.MediaType.Type == null || x.MediaType.Type == target.Type);
+
+                // If subtype provided then fuzzy or exact match reqired
+                if (target.SubType != null)
+                    query = query.Where(x => x.MediaType.SubType == null || x.MediaType.SubType == target.SubType);
+            }
+
+            return query
+                .Select(x => new
+                {
+                    x.MediaType,
+                    x.Writer,
+                    TypeMatch = x.MediaType.Type == target.Type,
+                    SubTypeMatch = x.MediaType.SubType == target.SubType
+                })
+                .Select(x => new
+                {
+                    x.MediaType,
+                    x.Writer,
+                    // every match increases the score
+                    Score = (x.TypeMatch ? 1 : 0) + (x.SubTypeMatch ? 1 : 0) + (target.Suffix == null && x.MediaType.Suffix == null ? 1 : 0)
+                })
+                .OrderByDescending(x => x.Score)
+                .Select(x => x.Writer)
+                .FirstOrDefault();
         }
     }
 
