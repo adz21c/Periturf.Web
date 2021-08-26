319ea84af28e1b2b4b970af955d853f247c366bf
Adam Burton <adz21c@gmail.com>
2021-08-26 22:58:49 +0100

    Mass licence header fix.

--- src/Periturf.Web/Configuration/Responses/IWebResponseConfigurator.cs
+++ src/Periturf.Web/Configuration/Responses/IWebResponseConfigurator.cs
@@ -1,7 +1,7 @@
 ﻿//
 //   Copyright 2021 Adam Burton (adz21c@gmail.com)
 //   
-//   Licensed under the Apache License, Version 2.0 (the "License");
+//   Licensed under the Apache License, Version 2.0 (the "License")
 //   you may not use this file except in compliance with the License.
 //   You may obtain a copy of the License at
 //   
